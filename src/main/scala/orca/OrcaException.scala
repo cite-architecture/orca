4e3d519d48eff8a6b610043cab33aa31677911b0
Neel Smith <dnsmith.neel@gmail.com>
2017-02-01 12:26:54 -0500

    Rm obsolete source

--- src/main/scala/orca/OrcaException.scala
+++
@@ -1,8 +0,0 @@
-package edu.holycross.shot
-package orca {
-
-  case class OrcaException(message: String = "", cause: Option[Throwable] = None) extends Exception(message) {
-    cause.foreach(initCause)
-  }
-
-}
